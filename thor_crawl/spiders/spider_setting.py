11deac7fcc6c7adac2cd102ed978f7d53a380af2
luoyanjie <546223592@qq.com>
2019-05-02 20:49:39 +0800

    lyj    1、房天下；

--- thor_crawl/spiders/spider_setting.py
+++ thor_crawl/spiders/spider_setting.py
@@ -1,6 +1,6 @@
 from thor_crawl.utils.db.daoUtil import DaoUtils
 from thor_crawl.utils.db.mysql.mySQLConfig import MySQLConfig
 
-DB_ENV = MySQLConfig.localhost()  # 数据库环境
+DB_ENV = MySQLConfig.inner()  # 数据库环境
 DB_PARAM = {'dbType': 'MySQL', 'config': DB_ENV}  # 数据库参数
 DEFAULT_DB_ENV = DaoUtils(**{'dbType': 'MySQL', 'config': DB_ENV})  # 默认使用的数据库
